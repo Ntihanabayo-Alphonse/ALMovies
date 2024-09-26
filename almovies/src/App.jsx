179cadf3c29da9051f0679b7d65b7dc7a98dedac
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-09-26 20:44:10 +0300

    import components

--- almovies/src/App.jsx
+++ almovies/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import MovieCard from './components/MovieCard';
+import { useState } from 'react';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const [ movieData, setMovieData ] = useState([]);
 
   return (
     <>
-      <div>App</div>
+      <SearchBar setMovieData={setMovieData} />
+      {/* <Home /> */}
+      <MovieCard movieData={movieData}  />
     </>
   )
 }
