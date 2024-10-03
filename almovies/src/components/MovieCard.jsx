20be74b580246c2171f077ad744bec3ed614bb5e
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-10-03 21:41:43 +0300

    worked on the movie cards responsiveness

--- almovies/src/components/MovieCard.jsx
+++ almovies/src/components/MovieCard.jsx
@@ -11,16 +11,18 @@ const MovieCard = ({ movieData, setMovieData }) => {
             <SearchBar setMovieData={setMovieData} />
             {movieData && (
                 <div className='movie_bg relative'>
-                    <div className='movie_container absolute top-[0%] left-0 w-full min-h-full px-16 pt-28 pb-16 grid grid-cols-4 gap-4'>
+                    <div className='movie_container absolute left-0 w-full min-h-full px-8 sm:px-16 pt-48 sm:pt-28 pb-16 grid xl:grid-cols-4 xl:px-10 lg:px-20 md:px-6 gap-4 lg:grid-cols-3 md:grid-cols-2'>
                         {movieData.map(movie => (
                             <Link to={`/movies/${movie.imdbID}`} >
-                                <div key={movie.imdbID} className="movie_card bg-purple-700 my-0 mx-auto relative w-[270px] h-[400] rounded-lg overflow-hidden">
-                                    <div className="movie_image w-[270px] h-[400] rounded-lg bg-green-700">
+                                <div key={movie.imdbID} className="movie_card my-0 mx-auto relative  max-[400px]:w-[270px] w-[340px] lg:w-[270px] h-[400] rounded-lg overflow-hidden">
+                                    <div className="movie_image max-[400px]:w-[270px] w-[340px] lg:w-[270px] h-[400] rounded-lg bg-green-700">
                                         <img src={movie.Poster} alt="Movie Poster" className='w-full h-full object-cover' />
                                     </div>
-                                    <div className="movie_txt absolute bottom-0 left-0 px-5 bg-[rgba(0,0,0,0.4)] w-full py-4">
-                                        <h3 className='font-bold text-[17px] text-white'>{movie.Title}</h3>
-                                        <p className='font-bold text-sm'>{movie.Year}</p>
+                                    <div className="movie_txt absolute bottom-0 left-0 bg-[rgba(0,0,0)] w-full">
+                                        <div className="movie_txt absolute bottom-0 left-0 px-5 bg-[rgba(0,0,0,0.1)] w-full py-4 backdrop-blur-[2px]">
+                                            <h3 className='font-bold text-[17px] text-white'>{movie.Title}</h3>
+                                            <p className='font-bold text-sm'>{movie.Year}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </Link>
