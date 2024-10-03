f947182fd125f4ea8e45bcd7ecce9b7b144bdd43
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-10-03 11:25:35 +0300

    implement routing to different components

--- almovies/src/App.jsx
+++ almovies/src/App.jsx
@@ -3,36 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import MovieCard from './components/MovieCard';
 import { useState } from 'react';
-import SearchBar from './components/SearchBar';
 import MovieDetails from './components/MovieDetails';
 
 function App() {
-  const [movieData, setMovieData] = useState([]);
+  let [movieData, setMovieData] = useState([]);
 
   return (
     <>
       <Router>
         <Routes>
-          {/* <Route path='/' element={(
-            <>
-              <SearchBar movieData={movieData} setMovieData={setMovieData} />
-              <Home />
-              <MovieCard movieData={movieData} />
-            </>
-          )} /> */}
-          <Route path='/movies' element={(
-            <>
-              <SearchBar movieData={movieData} setMovieData={setMovieData} />
-              <MovieCard movieData={movieData} />
-            </>
-          )} />
-
-          <Route path='/movies/:imdbID' element={(
-            <>
-              <SearchBar movieData={movieData} setMovieData={setMovieData} />
-              <MovieDetails movieData={movieData} />
-            </>
-          )} />
+          <Route path='/' element={<Home movieData={movieData} setMovieData={setMovieData} />} />
+          <Route path='/movies' element={<MovieCard movieData={movieData} setMovieData={setMovieData} />} />
+          <Route path='/movies/:imdbID' element={<MovieDetails movieData={movieData} setMovieData={setMovieData} />} />
         </Routes>
       </Router>
     </>
