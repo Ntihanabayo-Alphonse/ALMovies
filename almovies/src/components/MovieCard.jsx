bc74eeedc70d4990cf089b51e93cafad2ef5f9f8
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-10-03 13:22:34 +0300

    designed the movie cards for user-friendly

--- almovies/src/components/MovieCard.jsx
+++ almovies/src/components/MovieCard.jsx
@@ -10,18 +10,17 @@ const MovieCard = ({ movieData, setMovieData }) => {
         <>
             <SearchBar setMovieData={setMovieData} />
             {movieData && (
-                <div className='banner h-full'>
-                    <div className="movie_bg -z-10"></div>
-                    <div className='movie_container min-h-[100vh] absolute top-[40%] left-0 w-full px-16 md:px-24 grid grid-cols-4 gap-4 sm:top-[20%]'>
+                <div className='movie_bg relative'>
+                    <div className='movie_container absolute top-[0%] left-0 w-full min-h-full px-16 pt-28 pb-16 grid grid-cols-4 gap-4'>
                         {movieData.map(movie => (
                             <Link to={`/movies/${movie.imdbID}`} >
-                                <div key={movie.imdbID} className="movie_card my-0 mx-auto bg-purple-700">
-                                    <div className="movie_image">
-                                        <img src={movie.Poster} alt="Movie Poster" />
+                                <div key={movie.imdbID} className="movie_card bg-purple-700 my-0 mx-auto relative w-[270px] h-[400] rounded-lg overflow-hidden">
+                                    <div className="movie_image w-[270px] h-[400] rounded-lg bg-green-700">
+                                        <img src={movie.Poster} alt="Movie Poster" className='w-full h-full object-cover' />
                                     </div>
-                                    <div className="movie_txt">
-                                        <h3 className=''>{movie.Title}</h3>
-                                        <p>{movie.Year}</p>
+                                    <div className="movie_txt absolute bottom-0 left-0 px-5 bg-[rgba(0,0,0,0.4)] w-full py-4">
+                                        <h3 className='font-bold text-[17px]'>{movie.Title}</h3>
+                                        <p className='font-bold text-sm'>{movie.Year}</p>
                                     </div>
                                 </div>
                             </Link>
