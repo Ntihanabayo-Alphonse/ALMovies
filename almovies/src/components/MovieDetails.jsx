fb20a5a32799b254a8ecd034bf7d679465c5fb3a
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-10-04 12:00:12 +0300

    movie details mobile responsiveness

--- almovies/src/components/MovieDetails.jsx
+++ almovies/src/components/MovieDetails.jsx
@@ -29,19 +29,18 @@ const MovieDetails = ({ movieData, setMovieData }) => {
     return (
         <>
             <SearchBar setMovieData={setMovieData} />
-            <div className='banner h-full relative'>
-                <div className="detail_banner"></div>
-                <div key={detail?.imdbID} className='detail_card absolute left-1/2 -translate-x-1/2 -translate-y-1/2 h-[70%] w-[75%] sm:top-[55%] rounded-xl font-bold'>
-                    <div className="detail_background w-full h-full relative rounded-xl">
-                        <img src={detail?.Poster} alt="" className='w-1/2 h-full absolute right-0 rounded-xl object-cover' />
-                        <div className="gradient-overlay w-full h-full absolute top-0 left-0 rounded-xl"></div>
+            <div className='detail_banner relative'>
+                <div key={detail?.imdbID} className='detail_card absolute top-[80%] left-1/2 -translate-x-1/2 -translate-y-1/2 h-full w-[100%]  sm:h-[80%] md:h-[75%] sm:w-[95%] md:w-[80%]  xl:h-[70%] xl:w-[75%] sm:top-[55%] rounded-xl font-bold'>
+                    <div className="detail_background w-full h-full relative rounded-xl max-[640px]:min-h-[190%]">
+                        <img src={detail?.Poster} alt="" className='sm:w-1/2 w-full h-full absolute sm:right-0 bottom-0 rounded-xl object-cover' />
+                        <div className="gradient-overlay h-full w-full absolute top-0 left-0 rounded-xl"></div>
                     </div>
-                    <div className="details w-full pr-10 h-full rounded-tl-xl rounded-bl-xl overflow-hidden absolute top-0 left-0 grid grid-cols-2">
-                        <div className="details-img h-full w-10/12">
+                    <div className="details w-full sm:pr-10 sm:h-full rounded-xl overflow-hidden sm:rounded-tl-xl sm:rounded-bl-xl sm:overflow-hidden absolute top-0 left-0 grid grid-cols-1 sm:grid-cols-2 max-[640px]:px-16 max-[400px]:px-8 max-[640px]:py-10">
+                        <div className="details-img h-full w-full sm:w-[85%] md:w-[75%] xl:w-[55%] max-[640px]:rounded-xl overflow-hidden">
                             <img src={detail?.Poster} alt="" className='h-full object-cover w-full' />
                         </div>
-                        <div className="detail_info py-6 w-full">
-                            <h3 className='font-bold text-3xl font-[philosopher] text-white'>{detail?.Title}</h3>
+                        <div className="detail_info py-6 px-6 w-full sm:w-[60%] xl:w-[70%] sm:h-full sm:absolute sm:right-0">
+                            <h3 className='font-bold text-3xl font-[philosopher] text-white max-[400px]:text-2xl'>{detail?.Title}</h3>
                             <p className='font-bold mb-8 text-[rgba(221,221,221,0.6)]'>{detail?.Year}</p>
                             <p>{detail?.Genre}</p>
                             <p>{detail?.Country}</p>
